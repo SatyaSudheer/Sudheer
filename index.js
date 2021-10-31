898786351c8b3f0c3a7d16fc4265c488111446ad
Satya Sudheer <satya7sudheer@gmail.com>
2021-10-31 23:03:59 +0530

    Adding basic info

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+console.log(`
+Satya Sudheer 
+
+Ample of experience in the field of IT specializing in the internet development, Object Oriented Programming, Relational Database, Graphical User Interfaces and client/ server architecture using VB.Net, C#.Net, ASP.Net, HTML, XML, Java Script, Web Servers like Microsoft IIS, Databases like SQL Server, Oracle, Teradata, Application Servers like Geo-Server.
+
+🐣 Twitter: https://twitter.com/satya_sudheer
+📖 Github: https://github.com/satyasudheer
+`);
